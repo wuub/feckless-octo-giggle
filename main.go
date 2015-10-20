e2f6d56566fd7bb1d383dd0edc10ccbbd746c268
Wojciech Bederski <github@wuub.net>
2015-10-20 17:15:00 +0200

    basic streams working as expected

--- main.go
+++ main.go
@@ -2,11 +2,36 @@ package main
 
 import (
 	"bufio"
+	"fmt"
+	// "log"
+	"net"
+	// "net/http"
+	// _ "net/http/pprof"
 	"os"
 )
 
+func Listen(streams *StreamMap) {
+	ln, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		panic(err)
+	}
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("new connection")
+		go streams.Handle(conn)
+	}
+}
+
 func main() {
+	// go func() {
+	// log.Println(http.ListenAndServe(":6060", nil))
+	// }()
+
 	streams := NewStreamMap()
 	go streams.Attach(bufio.NewReader(os.Stdin))
+	go Listen(streams)
 	streams.Serve()
 }
--- streams.go
+++ streams.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 	"io"
+	"log"
+	"net"
 	"sync"
 	"sync/atomic"
+	"time"
 )
 
 type Stream struct {
@@ -15,12 +18,57 @@ type Stream struct {
 	processed uint64
 	filtered  uint64
 	sent      uint64
+	filter    Filter
+	sender    Sender
+	conn      net.Conn
+}
+
+func NewStream(conn net.Conn) *Stream {
+	s := new(Stream)
+	s.input = make(chan []byte, 100)
+	s.id = conn.RemoteAddr().String()
+	s.conn = conn
+	f, err := PrefixFilter("")
+	if err != nil {
+		panic(err)
+	}
+	s.filter = f
+	return s
+}
+
+func (s *Stream) Serve() (err error) {
+	var line []byte
+	ticker := time.NewTicker(5 * time.Second)
+	var now time.Time
+	for {
+		select {
+		case line = <-s.input:
+			if s.filter == nil {
+				continue
+			}
+			atomic.AddUint64(&s.processed, 1)
+			if !s.filter(line) {
+				atomic.AddUint64(&s.filtered, 1)
+				continue
+			}
+		case now = <-ticker.C:
+			line = []byte(fmt.Sprintf("%s heartbeat %d %d\n", now, s.delivered, s.dropped))
+		}
+		// TODO: for now we're ignoring short writes
+		_, err = s.conn.Write(line)
+		if err != nil {
+			return
+		}
+	}
+	return
 }
 
 type StreamMap struct {
-	stop    chan bool
-	input   chan []byte
-	streams map[string]*Stream
+	stop           chan bool
+	processed      uint64
+	processedBytes uint64
+	input          chan []byte
+	streams        map[string]*Stream
 	sync.RWMutex
 }
 
@@ -35,7 +83,8 @@ func NewStreamMap() *StreamMap {
 func (s *StreamMap) Deliver(line []byte) {
 	s.RLock()
 	defer s.RUnlock()
-	fmt.Println(line)
+	s.processed += 1
+	s.processedBytes += uint64(len(line))
 	for _, stream := range s.streams {
 		select {
 		case stream.input <- line:
@@ -50,12 +99,21 @@ func (s *StreamMap) Connect(stream *Stream) {
 	s.Lock()
 	defer s.Unlock()
 	s.streams[stream.id] = stream
+	log.Printf("Connected %s. Active streams: %d\n", stream.id, len(s.streams))
 }
 
 func (s *StreamMap) Disconnect(stream *Stream) {
 	s.Lock()
 	defer s.Unlock()
 	delete(s.streams, stream.id)
+	log.Printf("Disconnected %s. Active streams: %d\n", stream.id, len(s.streams))
+}
+
+func (s *StreamMap) Handle(conn net.Conn) {
+	stream := NewStream(conn)
+	s.Connect(stream)
+	defer s.Disconnect(stream)
+	stream.Serve()
 }
 
 func (s *StreamMap) Serve() {
@@ -74,10 +132,14 @@ func (s *StreamMap) Stop() {
 	s.stop <- true
 }
 
-func (s *StreamMap) Attach(r Readliner) {
+type ReadByteser interface {
+	ReadBytes(byte) ([]byte, error)
+}
+
+func (s *StreamMap) Attach(r ReadByteser) {
 	defer s.Stop()
 	for {
-		line, _, err := r.ReadLine()
+		line, err := r.ReadBytes('\n')
 		if err == io.EOF {
 			break
 		}
